dafa4dfaca344539dbc6de1fc08655e96d24a46e
Matt Surabian <matt@mattsurabian.com>
2014-03-13 11:09:02 -0400

    Update metadata and roles

--- cookbooks/custom-apache-config/metadata.rb
+++ cookbooks/custom-apache-config/metadata.rb
@@ -1,8 +1,8 @@
-name             'custom-config'
+name             'custom-apache-config'
 maintainer       'Matt Surabian'
 maintainer_email 'matt@mattsurabian.com'
 license          'All rights reserved'
-description      'Does custom server config'
+description      'Does custom apache server config'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
